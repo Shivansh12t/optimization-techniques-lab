be132c6e5354be5f007e367c84588e3d7e6ef962
Shivansh Tuteja <tuteja101@gmail.com>
2025-04-09 11:02:15 +0530

    simplex lab2 - practise question

---
+++ cb_simplex_method.m
@@ -0,0 +1,83 @@
+% Input Parameters
+c = [3 2];
+info = [1, 1; 2, 1];
+b = [6; 6];
+
+s = eye(size(info, 1)); % 2 Slack Variables due to the problem
+A = [info s b]; % Combined to make content of Simplex Table
+
+cost = zeros(1, size(A, 2)); % Cost is the size of columns of A
+cost(1:size(c, 2)) = c;
+
+Bv = size(c, 2) + 1:size(A, 2) - 1;
+
+% Calculate Zj-Cj
+zjcj = cost(Bv) * A - cost;
+zcj = [zjcj; A]; % Corrected variable name
+
+simplTable = array2table(zcj);
+simplTable.Properties.VariableNames = {'x_1', 'x_2', 's_1', 's_2', 'sol'}
+
+% Loop Over
+RUN = true;
+while RUN
+    % Next Step
+    if any(zjcj(1, 1:end-1) < 0)  % Check only Zj - Cj row (exclude last column)
+        fprintf("The Current BFS is not Optimal\n");
+    else
+        disp("Optimal Solution Reached");
+        RUN = false;
+    end
+    
+    % Find the Entering Variable (Most Negative Zj - Cj)
+    zc = zjcj(1, 1:end-1);  % Get all except the last column
+    [enter_col, pvt_col] = min(zc);  % Find the most negative value index
+    
+    % Find the Leaving Variable
+    sol = A(:, end); % Corrected: Take the last column (solution values)
+    column = A(:, pvt_col);
+    ratio = inf(size(column));  % Preallocate with inf
+    
+    for i = 1:size(column, 1)
+        if column(i) > 0
+            ratio(i) = sol(i) / column(i);
+        end
+    end
+    
+    % Find the Pivot Row (Minimum Ratio Rule)
+    [leave_row_value, pvt_row] = min(ratio);
+    
+    % Display Results
+    fprintf("Entering Variable: x_%d\n", pvt_col);
+    fprintf("Leaving Variable: x_%d\n", Bv(pvt_row));
+    fprintf("Pivot Element: %f\n", A(pvt_row, pvt_col));
+    
+    Bv(pvt_row) = pvt_col;
+    disp('New Basic Variable (Bv) =');
+    disp(Bv);
+    
+    % Pvt Key
+    pvt_key = A(pvt_row, pvt_col);
+    
+    % Update Table for Next Iteration
+    A(pvt_row, :) = A(pvt_row, :) ./ pvt_key;
+    
+    for i = 1:size(A,1)
+        if i ~= pvt_row
+            A(i,:) = A(i,:) - A(i, pvt_col) .* A(pvt_row,:);
+        end
+        zjcj = zjcj - zjcj(pvt_col) .* A(pvt_row,:);
+    end
+    
+    % Printing Purpose
+    zcj = [zjcj; A];
+    simptable = array2table(zcj);
+    simplTable.Properties.VariableNames = {'x_1', 'x_2', 's_1', 's_2', 'sol'}
+    
+    BFS = zeros(1, size(A,2));
+    BFS(Bv) = A(:, end);
+    BFS(end) = sum(BFS.*cost);
+    
+    current_BFS = array2table(BFS);
+    current_BFS.Properties.VariableNames = {'x_1', 'x_2', 's_1', 's_2', 'sol'}
+end
